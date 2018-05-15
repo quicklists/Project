5e8f0046ff6f175135eddce4ff283f5bf5e25caa
Bakr Al-Humaimidi <bakir_omer66@yahoo.com>
2018-05-15 12:09:40 -0700

    Unit testing

--- test.js
+++ test.js
@@ -1,56 +1,58 @@
+{
 /*
  * End to End Unit Testing
  */
 
-//let chai = require('chai');
-// Chai HTTP provides an interface for live integration testing
-//let chaiHttp = require('chai-http');
-let server = require('./server');
-//let should = chai.should();
-//let expect = chai.expect;
+    let chai = require('chai');
+    // Chai HTTP provides an interface for live integration testing
+    let chaiHttp = require('chai-http');
+    let server = require('./server');
+    let should = chai.should();
+    let expect = chai.expect;
 
-/*
- * Chai-http makes it very easy to test Node.js HTTP applications without having 
- * to to go through the hassle of finding a free port and start the service manually.
- * 
- * Tell chai to use chai-http.
- */
-//chai.use(chaiHttp);
+    /*
+     * Chai-http makes it very easy to test Node.js HTTP applications without having 
+     * to to go through the hassle of finding a free port and start the service manually.
+     * 
+     * Tell chai to use chai-http.
+     */
+    chai.use(chaiHttp);
 
-const binaryParser = function(res, cb) {
-    // res.setEncoding("text");
-    res.data = "";
-    res.on("data", function(chunk) {
-        res.data += chunk;
+    const binaryParser = function(res, cb) {
+        // res.setEncoding("text");
+        res.data = "";
+        res.on("data", function(chunk) {
+            res.data += chunk;
+        });
+        res.on("end", function() {
+            cb(null, res.data);
+        });
+    };
+
+    /*
+     * Waiting time for MongoDB to execute before running the test
+     */
+    /*describe('Server', () => {
+        beforeAll((done) => {
+            setTimeout(done, 4000);
+        });*/
+
+    /*
+     * POST Login Validation
+     */
+    describe.skip("POST /login", () => {
+        test("Should display error message", () => {
+            return chai.request(server)
+                .post('/login')
+                .send({ email: "example@email.com", password: "abcdef" })
+                .buffer().parse(binaryParser)
+                .then((res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.contain('Wrong email or password');
+                });
+        });
     });
-    res.on("end", function() {
-        cb(null, res.data);
-    });
-};
-
-/*
- * Waiting time for MongoDB to execute before running the test
- */
-/*describe('Server', () => {
-    beforeAll((done) => {
-        setTimeout(done, 4000);
-    });*/
-
-/*
- * POST Login Validation
- */
-describe.skip("POST /login", () => {
-    test("Should display error message", () => {
-        return chai.request(server)
-            .post('/login')
-            .send({ email: "example@email.com", password: "abcdef" })
-            .buffer().parse(binaryParser)
-            .then((res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.contain('Wrong email or password');
-            });
-    });
-});
+}
 
 var myDB = require("./connect");
 
@@ -92,38 +94,18 @@ var obj = {
     ]
 };
 
-describe("addRecord function testing", ()=>{
+describe.skip("addRecord function testing", ()=>{
     test("added product to a list", ()=>{
         myDB.addUserDb(obj,"Users", function(msg){
             expect(msg).toBe("success");
         })
     });
 });
-describe("deleteRecord function testing", ()=>{
+
+describe.only("deleteRecord function testing", ()=>{
     test("deleted product from a list", ()=>{
         myDB.deleteUserDb(obj,"Users", function(msg){
             expect(msg).toBe("success");
         })
     });
 });
-
-describe("dropCategory function testing", ()=>{
-    test("delete category inside list", ()=>{
-        myDB.deleteCategoryDb(obj,"Categories", function(msg){
-            expect(msg).toBe("success");
-        })
-    })
-})
-
-
-/*var listNameValidate = require("./validate.js");
-
-describe("list names testing", ()=>{
-    test("a valid list name with space", ()=>{
-        expect(listNameValidate("l i s t")).toBeFalsy();
-    });
-
-    test("a valid list name with case sensitive", ()=>{
-        expect(listNameValidate("List")).toBeTruthy();
-    });
-});*/
